d83a801077a4323c57d7c65baa602f01bcd6da2d
Ezra Cohen <ezrac1234@gmail.com>
2021-07-22 20:08:00 -0400

    Update vehicle_live_map.py

--- vehicle_live_map.py
+++ vehicle_live_map.py
@@ -1,13 +1,29 @@
+
 import json
 import folium
-with open('vehicles-location.json') as myfile:
-    vehicle_data=[myfile.read()]
+def main():
+    with open('vehicles-location.json') as myfile:
+        vehicle_data=[myfile.read()]
+    print(get_ids_locations(vehicle_data))
+#for i in vehicle_data:
+#    map = folium.Map(vehicle_data.value("001078f6-acfd-42fe-8c9c-84cecbb83630", 'location'))
+#map.save('html')
 
-dict(vehicle_data)
-map = folium.Map(vehicle_data.value("001078f6-acfd-42fe-8c9c-84cecbb83630", 'location'))
-map.save('html')
-class Vehicle:
-    def __init__():
-        id=
 
-    def
+
+def get_ids_locations(list_of_dicts):
+    ids=[]
+    latitudes=[]
+    longitudes=[]
+    for i in list_of_dicts:
+        for key, value in list_of_dicts.items():
+            if type(value) is dict and key=="location":
+                get_ids_locations(value)
+            else:
+                if key=="lat":
+                    latitudes.append(value)
+                elif key=="lng":
+                    longitudes.append(value)
+                elif key=="id":
+                    ids.append(value)
+    return ids, latitudes, longitudes
