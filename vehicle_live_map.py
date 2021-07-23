6d362e0a04909e40e416ecaec8066867790c57fa
Ezra Cohen <ezrac1234@gmail.com>
2021-07-23 09:06:41 -0400

    Update vehicle_live_map.py

--- vehicle_live_map.py
+++ vehicle_live_map.py
@@ -1,16 +1,23 @@
 import json
 import folium
+from folium.map import Icon, Popup
 
 def main():
     with open('vehicles-location.json') as myfile:
         vehicle_data=json.load(myfile)
-    print(len(vehicle_data))
+    #print(len(vehicle_data))
     crucial_car_data=get_ids_locations(vehicle_data)
+    vehicle_ids=crucial_car_data[0]
     vehicle_coordinates=get_lats_lngs(crucial_car_data[1])
     map=folium.Map(location=(51.5074, 0.1278), tiles="Stamen Terrain")
-    map.save("london.html")
     fg=folium.FeatureGroup(name="vehicle_map")
-    #for coordinates in 
+    
+    index=0
+    for lat, lng in zip(vehicle_coordinates[0], vehicle_coordinates[1]):
+        fg.add_child(folium.Marker(location=[lat, lng], Popup=vehicle_ids[index], icon=folium.Icon(color='green')))
+        index+=1
+    map.add_child(fg)
+    map.save("london.html")
 
 
 
@@ -37,5 +44,6 @@ def get_lats_lngs(list_of_dicts):
                 latitudes.append(v)
             elif k=="lng":
                 longitudes.append(v)
+    #coordinates=[{latitudes[i]: longitudes[i] for i in range(len(latitudes))}]
     return latitudes, longitudes
 main()
