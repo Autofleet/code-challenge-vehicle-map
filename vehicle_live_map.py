5856ce282f2b62cfd1a0bdb2a60890abc88bb03b
Ezra Cohen <ezrac1234@gmail.com>
2021-07-23 08:10:57 -0400

    Update vehicle_live_map.py

--- vehicle_live_map.py
+++ vehicle_live_map.py
@@ -1,29 +1,41 @@
-
 import json
 import folium
+
 def main():
     with open('vehicles-location.json') as myfile:
-        vehicle_data=[myfile.read()]
-    print(get_ids_locations(vehicle_data))
-#for i in vehicle_data:
-#    map = folium.Map(vehicle_data.value("001078f6-acfd-42fe-8c9c-84cecbb83630", 'location'))
-#map.save('html')
+        vehicle_data=json.load(myfile)
+    print(len(vehicle_data))
+    crucial_car_data=get_ids_locations(vehicle_data)
+    vehicle_coordinates=get_lats_lngs(crucial_car_data[1])
+    map=folium.Map(location=(51.5074, 0.1278), tiles="Stamen Terrain")
+    map.save("london.html")
+    fg=folium.FeatureGroup(name="vehicle_map")
+    #for coordinates in 
+
+
 
 
 
 def get_ids_locations(list_of_dicts):
     ids=[]
+    locations=[]
+    for d in list_of_dicts:
+        if type(d)==dict:
+            for key, value in d.items():
+                if key=="location":
+                    locations.append(value)
+                else: 
+                    if key=="id":
+                        ids.append(value)
+    return ids, locations
+def get_lats_lngs(list_of_dicts):
     latitudes=[]
     longitudes=[]
-    for i in list_of_dicts:
-        for key, value in list_of_dicts.items():
-            if type(value) is dict and key=="location":
-                get_ids_locations(value)
-            else:
-                if key=="lat":
-                    latitudes.append(value)
-                elif key=="lng":
-                    longitudes.append(value)
-                elif key=="id":
-                    ids.append(value)
-    return ids, latitudes, longitudes
+    for d in list_of_dicts:
+        for k, v in d.items():
+            if k=="lat":
+                latitudes.append(v)
+            elif k=="lng":
+                longitudes.append(v)
+    return latitudes, longitudes
+main()
