2c3a945d01b65d46c9ad67f95c86fe4a815fa1e6
Ezra Cohen <ezrac1234@gmail.com>
2021-07-25 11:45:10 -0400

    Update vehicle_live_map.py

--- vehicle_live_map.py
+++ vehicle_live_map.py
@@ -2,29 +2,6 @@ import json
 import folium
 import http.server
 import socketserver
-import flask
-from flask import Flask
-from flask_restful import Resource, Api, reqparse
-import pandas as pd
-import ast
-app = Flask(__name__)
-api = Api(app)
-
-class ids(Resource):
-    def get(self):
-        with open('vehicles-location.json') as myfile:
-            vehicle_data=json.load(myfile)
-        crucial_car_data=get_ids_locations(vehicle_data)
-        vehicle_ids=crucial_car_data[0]
-        return vehicle_ids, 200
-    pass
-
-class Locations(Resource):
-
-    pass
-
-api.add_resource(ids, '/ids')
-api.add_resource(Locations, '/Locations')
 
 
 def main():
@@ -32,19 +9,20 @@ def main():
         vehicle_data=json.load(myfile)
     crucial_car_data=get_ids_locations(vehicle_data)
     vehicle_coordinates=get_lats_lngs(crucial_car_data[1])
-    #map=folium.Map(location=(51.5074, 0.1278), tiles="Stamen Terrain")
-    #fg=folium.FeatureGroup(name="vehicle_map")
+    vehicle_ids=crucial_car_data[0]
+    map=folium.Map(location=(51.5074, 0.1278), tiles="Stamen Terrain")
+    fg=folium.FeatureGroup(name="vehicle_map")
     
-    #for lat, lng, id in zip(vehicle_coordinates[0], vehicle_coordinates[1], vehicle_ids):
-        #fg.add_child(folium.Marker(location=[lat, lng], popup=id, icon=folium.Icon(color='green')))
-    #map.add_child(fg)
-    #map.save("london.html")
-    #PORT=8080
-    #Handler=http.server.SimpleHTTPRequestHandler
+    for lat, lng, id in zip(vehicle_coordinates[0], vehicle_coordinates[1], vehicle_ids):
+        fg.add_child(folium.Marker(location=[lat, lng], popup=id, icon=folium.Icon(color='green')))
+    map.add_child(fg)
+    map.save("london.html")
+    PORT=8080
+    Handler=http.server.SimpleHTTPRequestHandler
 
-    #with socketserver.TCPServer(("", PORT), Handler) as httpd:
-        #print ("serving at port", PORT)
-        #httpd.serve_forever()
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        print ("serving at port", PORT)
+        httpd.serve_forever()
     
 
 def get_ids_locations(list_of_dicts):
@@ -70,5 +48,4 @@ def get_lats_lngs(list_of_dicts):
             elif k=="lng":
                 longitudes.append(v)
     return latitudes, longitudes
-if __name__ == '__main__':
-    app.run()
+main()
